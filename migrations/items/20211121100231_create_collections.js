d882809ad916b8f93cb7de6feb7b941d0710e06a
esfox <osairis100@gmail.com>
2021-11-21 19:36:58 +0800

    Create collections table

---
+++ migrations/items/20211121100231_create_collections.js
@@ -0,0 +1,28 @@
+const tableName = 'collections';
+
+/** @param {import('knex').Knex} knex */
+exports.up = async function(knex)
+{
+  if(await knex.schema.hasTable(tableName))
+    return;
+
+  await knex.schema.createTable(tableName, table =>
+  {
+    table.increments('pk_id');
+
+    table.string('code').notNullable().unique();
+    table.string('name').notNullable().index();
+    table.decimal('fixed_bonus').defaultTo(0);
+    table.text('items');
+
+    table.dateTime('created_at').index().defaultTo(knex.fn.now());
+    table.dateTime('updated_at').index();
+    table.dateTime('deleted_at').index();
+  });
+};
+
+/** @param {import('knex').Knex} knex */
+exports.down = async function(knex)
+{
+  await knex.schema.dropTableIfExists(tableName);
+};
