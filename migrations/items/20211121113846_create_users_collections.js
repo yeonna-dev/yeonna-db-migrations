c27180dd76593dc692aedb6fe26cfae733b9933f
esfox <osairis100@gmail.com>
2021-11-21 19:48:15 +0800

    Create users collections table

---
+++ migrations/items/20211121113846_create_users_collections.js
@@ -0,0 +1,27 @@
+const tableName = 'users_collections';
+
+/** @param {import('knex').Knex} knex */
+exports.up = async function(knex)
+{
+  if(await knex.schema.hasTable(tableName))
+    return;
+
+  await knex.schema.createTable(tableName, table =>
+  {
+    table.increments('pk_id');
+
+    table.string('user_id', 15).index().notNullable();
+    table.string('context').index();
+    table.string('collection_code').index().notNullable();
+
+    table.dateTime('created_at').index().defaultTo(knex.fn.now());
+    table.dateTime('updated_at').index();
+    table.dateTime('deleted_at').index();
+  });
+};
+
+/** @param {import('knex').Knex} knex */
+exports.down = async function(knex)
+{
+  await knex.schema.dropTableIfExists(tableName);
+};
