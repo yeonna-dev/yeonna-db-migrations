6b4cb0b7330fac4cc54f40c2cd60b442bca5cfab
esfox <osairis100@gmail.com>
2021-12-18 21:09:44 +0800

    Remove hard references in users bits table

--- migrations/bits/20210614112259_create_users_bits.js
+++ migrations/bits/20210614112259_create_users_bits.js
@@ -10,17 +10,8 @@ exports.up = async function(knex)
   {
     table.increments('pk_id');
 
-    table.string('user_id', 15)
-      .index()
-      .notNullable()
-      .references('id')
-      .inTable('users');
-
-    table.string('bit_id', 15)
-      .index()
-      .notNullable()
-      .references('id')
-      .inTable('bits');
+    table.string('user_id', 15).index().notNullable();
+    table.string('bit_id', 15).index().notNullable();
 
     table.dateTime('created_at').index().defaultTo(knex.fn.now());
     table.dateTime('updated_at').index();
