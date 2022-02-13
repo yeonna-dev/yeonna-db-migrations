8998227fd46f7388673579a803592618dd195b4b
esfox <osairis100@gmail.com>
2022-02-13 10:21:38 +0800

    Update items migration

--- migrations/items/20210630080635_create_items.js
+++ migrations/items/20210630080635_create_items.js
@@ -10,14 +10,15 @@ exports.up = async function(knex)
   {
     table.increments('pk_id');
 
-    table.string('category_id', 15).index();
     table.string('code').unique();
     table.string('name').unique();
+    table.decimal('price').defaultTo(0);
     table.decimal('chance_min');
     table.decimal('chance_max');
-    table.decimal('price').defaultTo(0);
     table.string('image');
     table.string('emote');
+    table.string('context').index();
+    table.string('category_id', 15).index();
 
     table.dateTime('created_at').index().defaultTo(knex.fn.now());
     table.dateTime('updated_at').index();
