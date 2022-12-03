6daf0b349d59f18f8592259ffd7d863887438664
esfox <osairis100@gmail.com>
2022-12-04 07:24:58 +0800

    Add migration script templates

---
+++ templates/create-table-migration.template.js
@@ -0,0 +1,30 @@
+// TODO: Set table name
+const tableName = 'TODO';
+
+/** @param {import('knex').Knex} knex */
+exports.up = async function(knex)
+{
+  if(await knex.schema.hasTable(tableName))
+    return;
+
+  await knex.schema.createTable(tableName, table =>
+  {
+    table.increments('pk_id');
+
+    // TODO: Add table columns
+    // e.g.
+    // table.string('user_id', 15).index().notNullable();
+    // table.string('context').index();
+    // table.integer('count').defaultTo(0);
+
+    table.dateTime('created_at').index().defaultTo(knex.fn.now());
+    table.dateTime('updated_at').index();
+    table.dateTime('deleted_at').index();
+  });
+};
+
+/** @param {import('knex').Knex} knex */
+exports.down = async function(knex)
+{
+  await knex.schema.dropTableIfExists(tableName);
+};
